8534b6d8cccd1b77d9a1c5487c8e302812533a51
Raghunadh Barlapudi <81347796+9394113857@users.noreply.github.com>
2026-05-12 18:14:11 +0530

    Add system status check on component initialization
    Implement OnInit lifecycle hook to check system status via API.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Angular_Test_App';
+
+  systemStatus = '🟡 System Warming Up';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+
+    this.http.get('https://django-restapi-r7yj.onrender.com/api/tasks/')
+      .subscribe({
+
+        next: () => {
+
+          this.systemStatus = '🟢 System Ready';
+
+        },
+
+        error: () => {
+
+          this.systemStatus = '🔴 System Offline';
+
+        }
+
+      });
+
+  }
+
 }
